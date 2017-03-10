1a82516ac875e1a3f4438e120c5c5d619b92a111
Peter 4A5A <498883@edu.rocmn.nl>
2017-03-10 09:37:56 +0100

    Chanced some json

--- json.class.php
+++ json.class.php
@@ -6,29 +6,35 @@
       // The object name contains the Name of the object
       // Object key contains the name of the array name of the object
       // Object value contains the value of the objectKey
-      $jsonObject = '
+      $jsonObject = '"
         myObj = {
           "file" : {
-            "name":"' . $fileName . '",
-            "fileType":"' . $fileType . '",
-            "content":"' . $fileContent . '"
+            "name":"' . $fileName . '" {
+              "fileType":"' . $fileType . '",
+              "content":"' . $fileContent . '"
+            }
+            "name": "sander" {
+              "fileType": "docx",
+              "content": "fsdfdsafdsafdsafdsa"
+            }
           }
         }
-      ';
+      "';
       return($jsonObject);
     }
     function decodeJson($sjonObject) {
       $jsonDecode = json_decode($sjonObject);
-      foreach ($jsonDecode as $key => $value) {
         echo "string";
-        if ($value->name == "peter") {
+        if ($jsonDecode->name == "peter") {
           echo $value->name;
-        }
       }
     }
   }
   $json = new json;
   $test = $json->createJsonObject("peter", "txt","gnjkfsnjkgf");
-  $json->decodeJson($test);
+  // $json->decodeJson($test);
+  echo $test;
+  echo json_decode($test);
+  var_dump(json_decode($test));
 
 ?>
